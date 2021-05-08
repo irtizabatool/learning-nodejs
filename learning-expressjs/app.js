3a6eded5e0d12e142bd153a8a23399d487d6f734
irtizabatool <irtizabatool@gmail.com>
2021-05-08 12:41:16 +0530

    Complete EJS templates with express

--- learning-expressjs/app.js
+++ learning-expressjs/app.js
@@ -4,17 +4,21 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+app.get('/:userQuery', (req,res) => {
+    res.render('index', {data: {
+        userQuery: req.params.userQuery,
+        searchResults: ['book1','book2', 'book3'],
+        loggedIn: false,
+        username: 'irtiza'
+    }});
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.json({success: true});
-});
+// app.post('/', (req, res) => {
+//     console.log(req.body);
+//     res.json({success: true});
+// });
 
 app.get('/ex', (req, res) => {
     res.send('hitting example route');
