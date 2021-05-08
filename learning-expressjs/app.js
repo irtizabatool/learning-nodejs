698b10a6b8a773702fdaccdd3e9db35e2eab9a1f
irtizabatool <irtizabatool@gmail.com>
2021-05-08 11:37:40 +0530

    Serve static files using express

--- learning-expressjs/app.js
+++ learning-expressjs/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path =  require('path');
 const app = express();
 
+app.use('/public', express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req,res) => {
-    res.send('hello from expressjs');
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
 app.get('/ex', (req, res) => {
